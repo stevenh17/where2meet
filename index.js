46a985b853aa5362aab9958b9943ada82ad0a6e5
steven huang <114288160+stevenh17@users.noreply.github.com>
2023-10-11 03:49:11 -0700

    Regenerated compromised api key

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
